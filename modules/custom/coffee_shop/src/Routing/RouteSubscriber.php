0e875e477be4ff46d382780ef46b049f6825d2d1
Ryan Weaver <ryan@thatsquality.com>
2019-04-08 15:03:36 -0400

    Step 23: Event: Generate route subscriber

---
+++ modules/custom/coffee_shop/src/Routing/RouteSubscriber.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Drupal\coffee_shop\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Class RouteSubscriber.
+ *
+ * Listens to the dynamic route events.
+ */
+class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterRoutes(RouteCollection $collection) {
+  }
+}
