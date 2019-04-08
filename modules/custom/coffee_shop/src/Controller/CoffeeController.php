5d16874ce2f9ee7c4f73d9729b8e6d1eb7b73e04
Ryan Weaver <ryan@thatsquality.com>
2019-04-08 15:03:27 -0400

    Step 9: Service: Use config() controller shortcut

--- modules/custom/coffee_shop/src/Controller/CoffeeController.php
+++ modules/custom/coffee_shop/src/Controller/CoffeeController.php
@@ -2,8 +2,15 @@
 
 namespace Drupal\coffee_shop\Controller;
 
-class CoffeeController {
+use Drupal\Core\Controller\ControllerBase;
+
+class CoffeeController extends ControllerBase {
   public function brewCoffee($type) {
+    if ($type === null) {
+      $type = $this->config('coffee_shop.default')
+        ->get('type');
+    }
+
     $text = sprintf('Ding! Your delicious %s is ready!', $type);
 
     return [
