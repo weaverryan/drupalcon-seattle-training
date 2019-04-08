b3eb457307db12be284d954977537cb46d1a012f
Ryan Weaver <ryan@thatsquality.com>
2019-04-08 15:03:33 -0400

    Step 19: Event: Fill in onKernelRequest

--- modules/custom/coffee_shop/src/EventSubscriber/CoffeeDrinkerSubscriber.php
+++ modules/custom/coffee_shop/src/EventSubscriber/CoffeeDrinkerSubscriber.php
@@ -2,6 +2,17 @@
 
 namespace Drupal\coffee_shop\EventSubscriber;
 
-class CoffeeDrinkerSubscriber {
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
+class CoffeeDrinkerSubscriber implements EventSubscriberInterface {
+  public function onKernelRequest()
+  {
+    drupal_set_message('Good morning request! Time for some coffee!');
+  }
+
+  public static function getSubscribedEvents() {
+    return [
+      'kernel.request' => 'onKernelRequest',
+    ];
+  }
 }
